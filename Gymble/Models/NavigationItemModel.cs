0dfc55d836958a6ae70d6d3f6b250073fb12e450
HyungJune Kim <hj615@cuuva.com>
2025-08-09 17:22:33 +0900

    feat: 좌측 네비게이션 메뉴 추가

---
+++ Gymble/Models/NavigationItemModel.cs
@@ -0,0 +1,18 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gymble.Models
+{
+    public class NavigationItemModel
+    {
+        public PackIconKind IconKind { get; set; } = PackIconKind.MonitorDashboard;
+        public string Label { get; set; } = string.Empty;
+        public string TagName { get; set; } = string.Empty;
+        public ICommand Command { get; set; } = null!;
+    }
+}
---
+++ Gymble/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,71 @@
+﻿using Gymble.Controls;
+using Gymble.Models;
+using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Gymble.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<NavigationItemModel> NavigationItems { get; }
+
+        private bool _isDrawerOpen;
+        public bool IsDrawerOpen
+        {
+            get => _isDrawerOpen;
+            set
+            {
+                if (_isDrawerOpen != value)
+                {
+                    _isDrawerOpen = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand ToDashboardViewCommand { get; }
+        public ICommand ToMemberLIstViewCommand { get; }
+        public ICommand ToAttendaceViewCommand { get; }
+        public ICommand ToProductViewCommand { get; }
+        public ICommand ToMembershipViewCommand { get; }
+        public ICommand ToSettingsViewCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            ToDashboardViewCommand = new RelayCommand(SwapView);
+            ToMemberLIstViewCommand = new RelayCommand(SwapView);
+            ToAttendaceViewCommand = new RelayCommand(SwapView);
+            ToProductViewCommand = new RelayCommand(SwapView);
+            ToMembershipViewCommand = new RelayCommand(SwapView);
+            ToSettingsViewCommand = new RelayCommand(SwapView);
+
+            NavigationItems = new ObservableCollection<NavigationItemModel>()
+            {
+                new NavigationItemModel { IconKind = PackIconKind.MonitorDashboard, Label = "대쉬보드", TagName="dashboard", Command = ToDashboardViewCommand },
+                new NavigationItemModel { IconKind = PackIconKind.ViewList, Label = "회원 목록", TagName="memberlist", Command = ToMemberLIstViewCommand },
+                new NavigationItemModel { IconKind = PackIconKind.ListStatus, Label = "출석 목록", TagName="attendace", Command = ToAttendaceViewCommand },
+                new NavigationItemModel { IconKind = PackIconKind.Gym, Label = "상품", TagName="product", Command = ToProductViewCommand },
+                new NavigationItemModel { IconKind = PackIconKind.CardMembership, Label = "멤버쉽", TagName="membership", Command = ToMembershipViewCommand },
+                new NavigationItemModel { IconKind = PackIconKind.Settings, Label = "설정", TagName="settings", Command = ToSettingsViewCommand }
+            };
+        }
+
+        private void SwapView(object obj)
+        {
+            string tag = obj as string;
+            Console.WriteLine(tag);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string? name = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
