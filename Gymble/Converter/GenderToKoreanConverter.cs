ad88c98de86c2c50f07ee1b511567ba67773a1b0
PlyoJay <109705417+PlyoJay@users.noreply.github.com>
2025-08-19 23:14:32 +0900

    refactor: 회원 목록 정보 출력 다듬기

---
+++ Gymble/Converter/GenderToKoreanConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Gymble.Converter
+{
+    public class GenderToKoreanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string gender)
+            {
+                return gender.Equals("Male") ? "남" : "여";
+            }
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value as string;
+        }
+    }
+}
