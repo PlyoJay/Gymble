94b4aa4dc56d43dd9378f7375ed28385ef08e0fa
PlyoJay <109705417+PlyoJay@users.noreply.github.com>
2026-03-01 12:50:12 +0900

    design: 페이징 요소 추가 (구현 X)

---
+++ Gymble/Converter/CurrentPageEqualsConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Gymble.Converter
+{
+    public class PageIndexToDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int idx)
+                return (idx + 1).ToString(); // 0 -> 1, 1 -> 2 ...
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (int.TryParse(value?.ToString(), out int display))
+                return display - 1; // 1 -> 0
+            return 0;
+        }
+    }
+
+    public class CurrentPageEqualsConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length >= 2 && values[0] is int buttonIndex && values[1] is int currentIndex)
+                return buttonIndex == currentIndex;
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
